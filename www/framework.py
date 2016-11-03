e6efcaa91885f53768c12dc6c87564348d5935fe
pheynix <pheynixdu@gmai.com>
2016-11-03 18:21:54 +0800

    add framework.py

---
+++ www/framework.py
@@ -0,0 +1,73 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import functools
+import inspect
+import asyncio
+import logging
+from importlib import import_module
+
+def get(path):
+	def decorator(fn):
+		@functools.wraps(fn)
+		def wrapper(*args, **kw):
+			return fn(*args, **kw)
+		wrapper.__path__ = path
+		wrapper.__method__ = 'GET'
+		return wrapper
+	return decorator
+
+
+def post(path):
+	def decorator(fn):
+		@functools.wraps(fn)
+		def wrapper(*args, **kw):
+			return fn(args, kw)
+		wrapper.__route__ = path
+		wrapper.__method__ = 'POST'
+		return wrapper
+	return decorator
+
+
+def add_route(app, fn):
+	method = getattr(fn, '__method__', None)
+	path = getattr(fn, '__path__', None)
+
+	if not method or not path:
+		raise ValueError('未使用@get或者@post: %s' % fn.__name__)
+	else if not inspect.iscoroutinefunction(fn) or not inspect.isnogeneratorfunction(fn):
+		fn = asyncio.coroutine(fn)
+
+	logging.info('adding route: %s %s to %s' % (method, path, fn.__name__))
+	app.router.add_route(method, path, fn)
+
+
+def add_routes(app, module_name)
+	mod = import_module_(module_name)
+	for attr in dir(mod):
+		if attr.startwith('_'):
+			continue
+		else:
+			fn = getattr(mod, attr)
+			if callable(fn):
+				method = getattr(fn, '__method__', None)
+				path = getattr(fn, '__path__', None)
+				if method and path:
+					add_route(app, attr)
+
+
+
+def import_module_(module_name):
+	n = module_name.rfind('.')
+	if n == (-1):
+		return import_module(module_name)
+	else:
+		raise ValueError('暂时只支持同包的模块导入')
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
