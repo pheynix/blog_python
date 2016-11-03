f05696fed2d491a5c0abc041340b6f83fd7238e6
pheynix <pheynixdu@gmai.com>
2016-11-03 15:21:41 +0800

    finish model

--- www/model.py
+++ www/model.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, BigInteger, String, Text, Boolean, DateTime, ForeignKey, create_engine
+from sqlalchemy.orm import sessionmaker, relationship
+from time import time
 
 Base = declarative_base()
 
@@ -11,32 +12,62 @@ class User(Base):
 	__tablename__ = 'user'
 
 	id = Column(Integer, primary_key=True, nullable=False)
-	email = Column(String(100))
-	username = Column(String(100))
-	password = Column(String(100))
-	is_admin = Column(Boolean)
-	avatar_url = Column(String(200))
-	created_at = Column(DateTime)
+	email = Column(String(100), nullable=False)
+	username = Column(String(100), nullable=False)
+	password = Column(String(100), nullable=False)
+	is_admin = Column(Boolean, default=False)
+	avatar_url = Column(String(200), default='/www/static/default_avatar.png')
+	created_at = Column(BigInteger, default=time)
 
-# class Blog(Base):
-# 	pass
+	blogs = relationship('Blog')
 
-# class Comment(Base):
-# 	pass
+class Blog(Base):
+	__tablename__ = 'blog'
+
+	id = Column(Integer, primary_key=True, nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	title = Column(String(100), nullable=False)
+	summary = Column(Text())
+	content = Column(Text())
+	created_at = Column(BigInteger, default=time)
+
+	comments = relationship('Comment')
 
 
-engine = create_engine('mysql+mysqlconnector://root:@localhost:3306/db_test')
+class Comment(Base):
+	__tablename__ = 'comment'
+	id = Column(Integer, primary_key=True, nullable=False)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	blog_id = Column(Integer, ForeignKey('blog.id'))
+	content = Column(Text(), nullable=False)
+	created_at = Column(BigInteger, default=time)
+
+
+engine = create_engine('mysql+mysqlconnector://root:@localhost:3306/db_test', echo=True)
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 
 
 def main():
 	session = Session()
-	user1 = User(username='pheynix', email='pheynixdu@gmail.com')
-	user2 = User(username='skye', email='skye@hbo.com')
+	user1 = User(username='joe', password='password1', email='joe@gmail.com')
+	user2 = User(username='skye', password='password2', email='skye@hbo.com')
 	session.add(user1)
 	session.add(user2)
 	session.commit()
+
+	blog1 = Blog(user_id=1, title='论吃饭的重要性')
+	blog2 = Blog(user_id=2, title='什么才是世界上最好的语言')
+	session.add(blog1)
+	session.add(blog2)
+	session.commit()
+
+	comment1 = Comment(user_id=2, blog_id=1, content='食屎啦你')
+	comment1 = Comment(user_id=1, blog_id=2, content='php不服来辩')
+	session.add(comment1)
+	session.add(comment2)
+	session.commit()
+
 	session.close()
 
 
